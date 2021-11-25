2e04441dad6c9d97a81baa6e1ee10fc926594960
Teerapon Nampakdee <teerapon@kkumail.com>
2021-11-25 20:02:19 +0700

    add show modal condition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted, provide, watch } from 'vue';
+import { computed, onMounted, provide, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { useModal } from '@/composables/useModal';
 import { useI18n } from '@/composables/useI18n';
@@ -24,9 +24,18 @@ const space = computed(() => {
   return spaces.value[key] ? spaces.value[key] : {};
 });
 
+const isOpenTermAndCondition = ref(false);
+
+const onClickAccept = () => {
+  isOpenTermAndCondition.value = false;
+  localStorage.setItem('ACCEPT_CONDITION', 'true');
+};
+
 onMounted(async () => {
   await loadLocale();
   init();
+  const accept = localStorage.getItem('ACCEPT_CONDITION');
+  isOpenTermAndCondition.value = accept != 'true';
 });
 
 watch(modalOpen, val => {
@@ -46,5 +55,37 @@ watch(modalOpen, val => {
     </div>
     <div id="modal" />
     <Notifications />
+  </div>
+  <div
+    v-show="isOpenTermAndCondition && !(app.loading || !app.init)"
+    style="
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.5);
+      z-index: 100;
+    "
+  >
+    <Block
+      class="text-center extra-icon-container"
+      style="
+        margin-bottom: 24px !important;
+        background-color: #fff !important;
+        width: 420px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      "
+    >
+      <div>
+        <h1>{{ $t('settings.terms') }}</h1>
+        <div>{{ $t('metaverse.condition') }}</div>
+        <UiButton @click="onClickAccept">{{ $t('agree') }}</UiButton>
+      </div>
+    </Block>
   </div>
 </template>
